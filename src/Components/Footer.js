fe47b14fad702f729601d624018497f9466c4699
faiz260 <59044640+faiz260@users.noreply.github.com>
2020-08-21 16:57:52 +0100

    Footer Updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import { FaInstagram } from "react-icons/fa";
 import { AiOutlineFacebook } from "react-icons/ai";
 import { FiPhone } from "react-icons/fi";
+import { HiOutlineMail } from "react-icons/hi";
 import { IconContext } from "react-icons";
 
 export const Footer = () => {
@@ -11,39 +12,61 @@ export const Footer = () => {
       <Container
         fluid
         style={{
-          marginTop: "50px",
-          margin: "0",
-          height: "400px",
+          marginTop: "150px",
+          height: "500px",
           backgroundColor: "rgb(0, 0, 100)",
+          paddingTop: "50px",
+          //   paddingBottom: "100px",
         }}
       >
         <Row>
           <Col xs={12}>
-            <h6 style={{ margin: "50px", color: "white", fontSize: "30px" }}>
+            <h6
+              style={{
+                float: "left",
+                margin: "10px",
+                color: "white",
+                fontSize: "30px",
+              }}
+            >
               Contact Us:{" "}
             </h6>
+          </Col>
+          <Col xs={12}>
             <IconContext.Provider
               value={{
                 color: "white",
-                style: { margin: "30px", width: "50px", height: "50px" },
+                style: { margin: "20px", width: "40px", height: "40px" },
               }}
             >
-              <a href="https://www.facebook.com/qmrcollections">
-                <AiOutlineFacebook />
-              </a>
-              <a href="https://www.instagram.com/qmr_collections/">
-                <FaInstagram />
-              </a>
-              <br />
-
-              <h6>
-                {" "}
-                <FiPhone />{" "}
-                <span style={{ color: "white", fontSize: "16px" }}>
+              <Col>
+                <a href="https://www.facebook.com/qmrcollections">
+                  <AiOutlineFacebook />
+                </a>
+              </Col>
+              <Col>
+                <a href="https://www.instagram.com/qmr_collections/">
+                  <FaInstagram />
+                </a>{" "}
+              </Col>
+              <Col>
+                <FiPhone style={{}} />{" "}
+              </Col>
+              <Col>  
+                <span style={{ color: "white", fontSize: "14px" }}>
                   {" "}
-                  03462772919{" "}
+                  03408318059{" "}
                 </span>
-              </h6>
+              </Col>
+              <Col>
+                <HiOutlineMail style={{ }} />
+              </Col>
+              <Col>  
+                <span style={{ color: "white", fontSize: "14px" }}>
+                  {" "}
+                  qmrcollections@gmail.com{" "}
+                </span>
+              </Col>
             </IconContext.Provider>
           </Col>
         </Row>
